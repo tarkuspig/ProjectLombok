d476edbd4ebf48545e19509cf72d665aad6b0779
Dan Cavin <dcavin87@gmail.com>
2025-02-13 12:41:01 +0000

    ost header added

--- src/main/java/com/example/ProjectLombok/controller/BeerController.java
+++ src/main/java/com/example/ProjectLombok/controller/BeerController.java
@@ -3,6 +3,7 @@ package com.example.ProjectLombok.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.example.ProjectLombok.model.Beer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -27,7 +28,10 @@ public class BeerController {
     public ResponseEntity handlePost(@RequestBody Beer beer){
         Beer savedBeer = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity(HttpStatus.CREATED);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", "api/v1/beer" + savedBeer.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
 
     }
 
