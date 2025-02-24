4207dd8e4d45562cd17a6803151f6810d21e0c8c
Dan Cavin <dcavin87@gmail.com>
2025-02-24 12:51:37 +0000

    MOCK_MVC_Update

--- src/test/java/com/example/ProjectLombok/controller/CustomerControllerTest.java
+++ src/test/java/com/example/ProjectLombok/controller/CustomerControllerTest.java
@@ -19,8 +19,9 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CustomerController.class)
@@ -56,6 +57,18 @@ class CustomerControllerTest {
                         .content(objectMapper.writeValueAsString(customer)))
                 .andExpect(status().isCreated())
                 .andExpect(header().exists(("Location")));
+    }
+    @Test
+    void testUpdateCustomer() throws Exception {
+        Customer customer = customerServiceImpl.listCustomers().get(0);
+
+        mockMvc.perform(put("/api/v1/customer/" + customer.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customer)))
+                .andExpect((status().isNoContent()));
+
+        verify(customerService).updateCustomerById(any(UUID.class), any(Customer.class));
     }
     @Test
     void testListCustomers() throws Exception{
