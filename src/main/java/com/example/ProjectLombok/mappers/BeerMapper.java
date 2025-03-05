b27359e24d1794d142e67e6cf942a9dbcb81bd23
Dan Cavin <dcavin87@gmail.com>
2025-03-05 16:05:11 +0000

    Mapper

--- src/main/java/com/example/ProjectLombok/mappers/BeerMapper.java
+++ src/main/java/com/example/ProjectLombok/mappers/BeerMapper.java
@@ -2,7 +2,9 @@ package com.example.ProjectLombok.mappers;
 
 import com.example.ProjectLombok.entities.Beer;
 import com.example.ProjectLombok.model.BeerDTO;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface BeerMapper {
     Beer beerDtoToBeer(BeerDTO dto);
 
--- src/main/java/com/example/ProjectLombok/mappers/CustomerMapper.java
+++ src/main/java/com/example/ProjectLombok/mappers/CustomerMapper.java
@@ -2,7 +2,9 @@ package com.example.ProjectLombok.mappers;
 
 import com.example.ProjectLombok.entities.Customer;
 import com.example.ProjectLombok.model.CustomerDTO;
+import org.mapstruct.Mapper;
 
+@Mapper
 public interface CustomerMapper {
 
     Customer customerDtoToCustomer(CustomerDTO dto);
